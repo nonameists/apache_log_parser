f922f084c3d094558796056ed1aead48ccb5208a
Tarakanov Oleg <nonameists@gmail.com>
2020-07-15 09:51:13 +0300

    add db to admin panel

--- apache_log/log_parser/admin.py
+++ apache_log/log_parser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import LogData
 
-# Register your models here.
+
+class LogDataSitesAdmin(admin.ModelAdmin):
+    list_display = ("pk", "ip", "date", "http_method", "url", "response_code", "response_size")
+    search_fields = ( "ip", "date", "http_method", "url", "response_code", "response_size")
+    empty_value_display = '-empty-'
+
+
+admin.site.register(LogData, LogDataSitesAdmin)
